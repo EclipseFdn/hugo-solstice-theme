6cdc7434c49599ca5fcc4a60e9df926dc684014c
Christopher Guindon <chris.guindon@eclipse-foundation.org>
2018-04-30 17:05:27 -0400

    Update copyright
    Signed-off-by: Christopher Guindon <chris.guindon@eclipse-foundation.org>

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,16 +1,14 @@
 let mix = require('laravel-mix');
-mix.options({uglify: {uglifyOptions: {compress: false, mangle: false, output: {comments: true}}}});
+mix.options({uglify: {uglifyOptions: {compress: false, output: {comments: true}}}});
 
 mix.setPublicPath('static');
 mix.setResourceRoot('../');
 
-mix.less('./node_modules/solstice-assets/less/quicksilver/styles.less', 'static/css/quicksilver.css');
-mix.less('./node_modules/solstice-assets/less/quicksilver/jakarta/styles.less', 'static/css/jakarta.css');
-mix.less('./node_modules/solstice-assets/less/quicksilver/eclipse-ide/styles.less', 'static/css/eclipse-ide.css');
+mix.less('./node_modules/eclipsefdn-solstice-assets/less/quicksilver/styles.less', 'static/css/styles.css');
 
 mix.scripts([
     './node_modules/jquery/dist/jquery.min.js',
     './node_modules/bootstrap/dist/js/bootstrap.min.js',
     './node_modules/feather-icons/dist/feather.min.js',
-    './src/javascript/solstice.js'
+    './node_modules/eclipsefdn-solstice-assets/js/solstice.js'
 ], './static/js/solstice.js');
