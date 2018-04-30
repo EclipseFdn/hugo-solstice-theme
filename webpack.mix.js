ac38bc36f667b527eb77da1616e9c8aa4b29912c
Christopher Guindon <chris.guindon@eclipse-foundation.org>
2018-04-29 20:56:20 -0400

    refactor hugo-solstice-theme
    In the process of migrating to laravel-mix and webpack for our assets.
    
    Signed-off-by: Christopher Guindon <chris.guindon@eclipse-foundation.org>

---
+++ webpack.mix.js
@@ -0,0 +1,14 @@
+let mix = require('laravel-mix');
+mix.options({uglify: {uglifyOptions: {compress: false, mangle: false, output: {comments: true}}}});
+mix.setPublicPath('static');
+
+mix.less('./node_modules/solstice-assets/less/quicksilver/styles.less', 'static/css/quicksilver.css');
+mix.less('./node_modules/solstice-assets/less/quicksilver/jakarta/styles.less', 'static/css/jakarta.css');
+mix.less('./node_modules/solstice-assets/less/quicksilver/eclipse-ide/styles.less', 'static/css/eclipse-ide.css');
+
+mix.scripts([
+    './node_modules/jquery/dist/jquery.min.js',
+    './node_modules/bootstrap/dist/js/bootstrap.min.js',
+    './node_modules/feather-icons/dist/feather.min.js',
+    './src/javascript/solstice.js'
+], './static/js/solstice.js');
