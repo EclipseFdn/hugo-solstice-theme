8a907b054283dcbc23fc31911c84ae2d9652d011
Christopher Guindon <chris.guindon@eclipse-foundation.org>
2018-07-30 13:00:51 -0400

    update solstice-assets and webpack
    Signed-off-by: Christopher Guindon <chris.guindon@eclipse-foundation.org>

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,9 +10,10 @@ mix.scripts([
     './node_modules/jquery/dist/jquery.min.js',
     './node_modules/bootstrap/dist/js/bootstrap.min.js',
     './node_modules/cookieconsent/src/cookieconsent.js',
-    './node_modules/feather-icons/dist/feather.min.js',
     './node_modules/eclipsefdn-solstice-assets/js/solstice.cookieconsent.js',
-    './node_modules/eclipsefdn-solstice-assets/js/solstice.cookies.js',
     './node_modules/eclipsefdn-solstice-assets/js/eclipsefdn.videos.js',
+    './node_modules/jquery-match-height/dist/jquery.matchHeight-min.js',
+    './node_modules/feather-icons/dist/feather.min.js',
+    './node_modules/eclipsefdn-solstice-assets/js/solstice.cookies.js',
     './node_modules/eclipsefdn-solstice-assets/js/solstice.js'
 ], './static/js/solstice.js');
