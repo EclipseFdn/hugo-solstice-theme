7623fe6fafebbd94b4319302f1f306d39af8a777
Martin Lowe <martin.lowe@eclipse-foundation.org>
2021-05-18 14:19:29 -0400

    Update to newest version of solstice assets

--- static/js/solstice.hugo.js
+++ static/js/solstice.hugo.js
@@ -1,17 +1,15 @@
-"use strict";
-
 // only use this code if jquery has been defined
-(function ($, document) {
-  // check to make sure that we have a table class to set
-  if (typeof tableClasses !== 'undefined' && tableClasses != null) {
-    // on document ready, inject table classes
-    $(document).ready(function () {
-      $('main table').each(function (index) {
-        // dont add classes if there are already some set
-        if ($(this).attr("class") == null) {
-          $(this).addClass(tableClasses);
-        }
-      });
-    });
-  }
+(function($, document) {
+	// check to make sure that we have a table class to set
+	if (typeof tableClasses !== 'undefined' && tableClasses != null) {
+		// on document ready, inject table classes
+		$(document).ready(function() {
+			$('main table').each(function(index) {
+				// dont add classes if there are already some set
+				if ($(this).attr("class") == null) {
+					$(this).addClass(tableClasses);
+				}
+			});
+		});
+	}
 })(jQuery, document);
