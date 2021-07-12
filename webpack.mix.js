3823330b00c3a88b66e8582a13d77705b3e2f8c2
Christopher Guindon <chris.guindon@eclipse-foundation.org>
2021-07-12 12:27:11 -0400

    upgrade js codebase
    Signed-off-by: Christopher Guindon <chris.guindon@eclipse-foundation.org>

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,25 +6,12 @@ mix.setResourceRoot('../');
 
 mix.less('./node_modules/eclipsefdn-solstice-assets/less/quicksilver/styles.less', 'static/css/styles.css');
 
-mix.babel([
-    './node_modules/eclipsefdn-solstice-assets/js/eclipsefdn.adopters.js'
-], './static/js/eclipsefdn.adopters.js');
+mix.webpackConfig({
+  resolve: {
+    alias: {
+      jquery: 'jquery/src/jquery',
+    },
+  },
+});
 
-mix.scripts([
-    './node_modules/jquery/dist/jquery.min.js',
-    './node_modules/bootstrap/dist/js/bootstrap.min.js',
-    './node_modules/cookieconsent/src/cookieconsent.js',
-    './node_modules/eclipsefdn-solstice-assets/js/solstice.cookieconsent.js',
-    './node_modules/eclipsefdn-solstice-assets/js/eclipsefdn.videos.js',
-    './node_modules/jquery-match-height/dist/jquery.matchHeight-min.js',
-    './node_modules/jquery-eclipsefdn-api/dist/jquery.eclipsefdn-api.min.js',
-    './node_modules/feather-icons/dist/feather.min.js',
-    './node_modules/owl.carousel/dist/owl.carousel.min.js',
-    './node_modules/eclipsefdn-solstice-assets/js/solstice.cookies.js',
-    './node_modules/eclipsefdn-solstice-assets/js/solstice.js',
-    './node_modules/eclipsefdn-solstice-assets/js/solstice.eventsmodal.js'
-], './static/js/solstice.js');
-
-mix.scripts([
-    './node_modules/eclipsefdn-solstice-assets/js/solstice.tables.js'
-], './static/js/solstice.hugo.js');
+mix.js(['js/main.js'], './static/js/solstice.js');
